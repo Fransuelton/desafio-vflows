317edbef8e34abb07a9b9644f6466af33f8e275d
Fransuelton <elton6103@gmail.com>
2023-10-13 10:02:22 -0300

    feat: adds download option and saves the attachment to blob and sessionStorage and changed names

--- src/scripts/interface/attachedDocuments.js
+++ src/scripts/interface/attachedDocuments.js
@@ -1,37 +1,49 @@
 import $ from "jquery";
 import removeImage from "../../images/trash.jpg";
-import viewImage from "../../images/eye.jpg";
+import downloadImage from "../../images/eye.jpg";
 
 $(document).ready(function () {
   let documentCount = 1;
+  let attachments = [];
 
-  $("#add-document").click(function () {
-    const documentName = "Documento em anexo " + documentCount;
+  $("#add-document").change(function () {
+    const files = $(this)[0].files;
 
-    const newDocument = $(`
-    <div class="fs-display-flex fs-align-items-center">
+    for (let i = 0; i < files.length; i++) {
+      const documentName = "Documento em anexo " + documentCount;
+      const file = files[i];
+      const fileURL = URL.createObjectURL(file);
+      const blob = new Blob([file], { type: file.type });
 
-      <button class='removeDocument btn-link'>
-        <img src="${removeImage}" title="Excluir Anexo">
-      </button>
+      attachments.push({ name: documentName, blob });
 
-      <button class="btn-link">
-        <img src="${viewImage}" title="Visualizar Anexo">
-      </button>
+      const newDocument = $(`
+        <div class="fs-display-flex fs-align-items-center">
+          <button class='removeDocument btn-link'>
+            <img src="${removeImage}" title="Excluir Anexo">
+          </button>
+          <a class="btn-link" href="${fileURL}" download="${documentName}">
+            <img src="${downloadImage}" title="Baixar Anexo">
+          </a>
+          <p class="fs-font-bold fs-text-lg fs-color-black fs-xs-margin">${documentName}</p>
+        </div>`);
 
-        <p class="fs-font-bold fs-text-lg fs-color-black">${documentName}</p>
-        
-    </div>`);
-    
-    newDocument.appendTo("#documentRegister");
-    documentCount++;
+      newDocument.appendTo("#documentRegister");
+      documentCount++;
 
-    newDocument.find(".removeDocument").click(function () {
-      $(this).closest(".fs-display-flex").remove();
+      newDocument.find(".removeDocument").click(function () {
+        URL.revokeObjectURL(fileURL);
+        attachments = attachments.filter(
+          (attachment) => attachment.name !== documentName
+        );
+        $(this).closest(".fs-display-flex").remove();
 
-      if ($("#documentRegister .fs-display-flex").length === 0) {
-        documentCount = 1;
-      }
-    });
+        if ($("#documentRegister .fs-display-flex").length === 0) {
+          documentCount = 1;
+        }
+      });
+    }
+
+    sessionStorage.setItem("attachments", JSON.stringify(attachments));
   });
 });
